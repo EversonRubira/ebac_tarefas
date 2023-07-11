7719c6d6c95e4b644138284f28ad0b8f4513fd52
Everson_Rubira <85372823+EversonRubira@users.noreply.github.com>
2023-07-11 09:22:02 +0100

    Delete Mod19Reflection directory

--- Mod19Reflection/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-a
-1src/pt/com/erubira/reflection/ReflectionTask.java,b\b\bb8e548359ea05868391cc5d23e904b062a60816
--- Mod19Reflection/src/pt/com/erubira/reflection/ReflectionTask.java
+++
@@ -1,9 +0,0 @@
-package pt.com.erubira.reflection;
-
-public class ReflectionTask {
-    public static void main (String [] args) throws NoSuchMethodException {
-        Class<ClassWithAnnotation> clazz = ClassWithAnnotation.class;
-
-        if(clazz.isAnnotationPresent(FirsAnnotation.class))
-    }
-}
