d93876493001677285304a6b4fe8b3191e117769
EversonRubira <eversonrubira@gmail.com>
2023-08-14 22:40:26 +0100

    Subindo Tarefa Teste Mod23SeparandoPorGeneroTest

--- Mod22SeparandoPorGenero/src/pt/com/rubira/SelecionandoGenero.java
+++ Mod22SeparandoPorGenero/src/pt/com/rubira/SelecionandoGenero.java
@@ -1,41 +1,43 @@
+package pt.com.rubira;
+
 import java.util.*;
 
-public class SeparandoPorGeneroMod22Lambdas {
+public class testGeneroFeminino {
 
-    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         List<String> nomesMasculinos = new ArrayList<>();
         List<String> nomesFemininos = new ArrayList<>();
         List<String> nomesNaoBinario = new ArrayList<>();
 
-        while (true) {
-            System.out.print("Digite o nome completo: ");
-            String nomeCompleto = scanner.nextLine();
-
-            if (nomeCompleto.trim().isEmpty()) {
-                System.out.println("Por favor digite um nome valido.");
-                continue;
-            }
-            System.out.print("Digite o gênero (M - Masc / F - Fem / NB - Nao Binario ): ");
-            String genero = scanner.nextLine();
+        public void adicionarNome(String nomeCompleto, String genero){
 
             if (genero.equalsIgnoreCase("M")) {
                 nomesMasculinos.add(nomeCompleto);
             } else if (genero.equalsIgnoreCase("F")) {
                 nomesFemininos.add(nomeCompleto);
-            }else if (genero.equalsIgnoreCase("NB")) {
+            } else if (genero.equalsIgnoreCase("NB")) {
                 nomesNaoBinario.add(nomeCompleto);
 
-            }else {
+            } else {
                 System.out.println("Gênero inválido! Tente novamente.");
-                continue;
+
             }
+        }
 
-            System.out.println("Deseja adicionar mais algum nome na lista? (Sim/Nao)");
-            String resposta = scanner.nextLine();
+        public List<String> getNomesFeminios() {
+            return nomesFemininos;
+        }
 
-            if (!resposta.equalsIgnoreCase("Sim")) {
-                break;  }
+        public List<String> getNomesMasculinos() {
+            return nomesMasculinos;
+        }
+
+        public List<String> getNomesNaoBinario() {
+            return nomesNaoBinario;
+        }
+
+        public void imprimirNomes() {
 
             System.out.println("Nomes de pessoas Nao Binario:");
             for (String nome : nomesNaoBinario) {
@@ -51,12 +53,36 @@ public class SeparandoPorGeneroMod22Lambdas {
             for (String nome : nomesMasculinos) {
                 System.out.println(nome);
             }
-
         }
-        scanner.close();
+
+        public static void main(String[] args) {
+            
+
+            while (true) {
+                System.out.print("Digite o nome completo: ");
+                String nomeCompleto = scanner.nextLine();
+
+                if (nomeCompleto.trim().isEmpty()) {
+                    System.out.println("Por favor digite um nome valido.");
+                    continue;
+                }
+                System.out.print("Digite o gênero (M - Masc / F - Fem / NB - Nao Binario ): ");
+                String genero = scanner.nextLine();
+
+            }
+                System.out.println("Deseja adicionar mais algum nome na lista? (Sim/Nao)");
+                String resposta = scanner.nextLine();
+
+                if (!resposta.equalsIgnoreCase("Sim")) {
+                    break;
+                }
+
+                scanner.close();
+
+            }
 
 
-        System.out.println("************************");
+            System.out.println("************************");
 
 
         System.out.println("pessoas do Genero Feminino: " );
